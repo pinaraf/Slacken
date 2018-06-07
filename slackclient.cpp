76c74488f10ae5597192af79e81d477e9f368d01
Pierre Ducroquet <pinaraf@pinaraf.info>
2018-06-07 22:32:00 +0200

    Add missing license file/headers

--- slackclient.cpp
+++ slackclient.cpp
@@ -1,3 +1,20 @@
+/**
+    This file is part of Slacken.
+
+    Slacken is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Slacken is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Slacken.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 #include "slackclient.h"
 #include <QDebug>
 #include <QOAuthHttpServerReplyHandler>
